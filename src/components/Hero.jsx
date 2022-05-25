390cb267f8c3ee6b19897468cb9d32dfc11e273c
Leon Antoshyn <Doctorsprait@gmail.com>
2022-05-25 14:14:30 +0300

    slick slider added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import ReactCustomArrow from './Slider'
 
 const Hero  = () => {
   return (
     <div className="hero">
         <div className="container">
-            
+            <ReactCustomArrow/>
         </div>
     </div>
   )
